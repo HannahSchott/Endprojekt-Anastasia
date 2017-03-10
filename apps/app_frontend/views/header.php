91736e3481bf0a71609a3682ab82ccfda916197b
Hannah Schott <hannah.schott@hotmail.com>
2017-03-10 15:43:39 +0100

    Header meta tags + favicon

--- apps/app_frontend/views/header.php
+++ apps/app_frontend/views/header.php
@@ -2,10 +2,12 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>SAE MVC</title>
+    <title>Anastasia</title>
     <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo APP_ROOT; ?>public/css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="../public/img/favicon.png">
 </head>
 <body>
   <header>
