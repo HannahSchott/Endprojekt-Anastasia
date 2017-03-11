f4e13c88d1b22c47888eb9b4a418b40aec801790
Hannah Schott <hannah.schott@hotmail.com>
2017-03-11 19:54:19 +0100

    Type berechnen teil 2

--- models/anastasia_model.php
+++ models/anastasia_model.php
@@ -52,13 +52,31 @@ class anastasia_model extends model{
   public function setAnswers()
   {
     //Aus session user id
-    // $user_id = sessions::get['user_id']
-
-    $user_id = '40';
+    $user_id = sessions::get('uid');
     $answers = $_POST['answers'];
+    $count = $_POST['count'];
+    //Count = 10, tortzdem Princess?
+    var_dump($count);
+    if($count > 15 || $count < 4){
+      return false;
+    }else{
+        if($count >= 4 && $count<=7){
+          //Princess
+          var_dump('Princess');
+          $box_type_id = '2';
+        }elseif($count >=8 && $count<=11){
+          //Queen
+          var_dump('Queen');
+          $box_type_id ='1';
+        }elseif($count >=12 && $count<=15){
+          //Mistress
+          var_dump('Mistress');
+          $box_type_id = '3';
+        }
+    }
 
-    $stmt = $this -> db -> prepare("UPDATE users SET box_answers = ? WHERE id = ? ");
-    $stmt -> bind_param("si", $answers,$user_id);
+    $stmt = $this -> db -> prepare("UPDATE users SET box_answers = ?, `box-type_id` = ? WHERE id = ? ");
+    $stmt -> bind_param("sii", $answers,$box_type_id,$user_id);
     $stmt -> execute();
     $stmt -> close();
 
